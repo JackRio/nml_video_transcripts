47e0828b10a4c91c156b35c2d034d6cd2ec3f5e5
Britt van Gemert <britt-van-gemert@live.nl>
2021-03-08 15:09:31 +0100

    removed time notations

--- js/button.js
+++ js/button.js
@@ -5,13 +5,13 @@
 // }
 
 document.addEventListener('DOMContentLoaded', function(){
-    document.querySelector('button').addEventListener('click',
-    onclick, false)
+    // Only execute the function when button is clicked
+    document.querySelector('button').addEventListener('click', onclick, false)
 
     function onclick(){
         chrome.tabs.query({active: true, currentWindow: true}, 
         function(tabs) {
-            chrome.windows.create({url: chrome.extension.getURL("../index.html"), type: "popup", top:20, left: 30, width: 300, height: 300});
+            chrome.windows.create({url: chrome.extension.getURL("../index.html"), type: "popup", top:20, left: 30, width: 600, height: 300});
         });
     }
 }, false)
--- js/index.js
+++ js/index.js
@@ -1,16 +1,26 @@
+/*
+Get text file content
+*/
 var request = new XMLHttpRequest();
 request.open('GET', './Example_transcript.txt', false);
 request.send();
 var textfileContent = request.responseText;
 
+/*
+Extract text, add hyperlinks and color the words red
+Remove time notations
+*/
 window.onload = function(){
     document.getElementById("transcript").innerHTML = textfileContent;
     var text = document.getElementById("transcript");
-    var str = text.innerHTML,
-        reg = /page/ig; //g is to replace all occurences
+    var str = text.innerHTML
+        //, reg = /page/ig; //g is to replace all occurences
         // multiple words: reg = /red|blue|green|orange/ig; 
 
-    // replace word from regex back in original string + add hyperlink
+    // replace time notations with whitespace
+    str = str.replace(/\d{2}:\d{2}/g, " ");
+
+    // replace word from regex with hyperlink + color
     str = str.replace(/page/g, '<a href="https://www.google.com" target="_blank" style="color:red;">chrome</a>');
 
     // multiple words need splitting:
