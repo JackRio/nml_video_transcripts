06c5bf92f840825c60492a326b23e85e8d9dc720
Britt van Gemert <britt-van-gemert@live.nl>
2021-05-20 11:51:50 +0200

    css fixed interfering web pages

--- js/index.js
+++ js/index.js
@@ -290,6 +290,7 @@ window.onload = function(){
         var element = document.createElement('a');
 
         element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+        
         element.setAttribute('download', filename);
          
         element.style.display = 'none';
